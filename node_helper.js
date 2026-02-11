2dcf8f55fb507288b0b9a9dc3987c898eb7b180d
Andreas <41864446+a-waider@users.noreply.github.com>
2026-02-11 10:35:12 +0000

    fix: api path

--- node_helper.js
+++ node_helper.js
@@ -60,7 +60,7 @@ module.exports = NodeHelper.create({
       const toDate = new Date(fromDate)
       toDate.setDate(toDate.getDate() + 7)
       params.append("to_date", toDate)
-      const mealplan = parseMealPlan(JSON.parse(fetch(`${payload.url}?${params}`, { headers: { Authorization: payload.token } })))
+      const mealplan = parseMealPlan(JSON.parse(fetch(`${payload.url}/api/meal-plan?${params}`, { headers: { Authorization: payload.token } })))
       console.debug("Got mealplan from Tandoor", mealplan)
       this.sendSocketNotification("TANDOOR_MEALPLAN", mealplan)
     }
